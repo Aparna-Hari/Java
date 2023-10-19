016864c78a0af76a884d78b50079bcfbc97ad48d
Aparna-Hari <hvaparna88@gmail.com>
2023-10-19 16:55:05 -0700

    Using + and concat()

--- StringConcatenation.java
+++ StringConcatenation.java
@@ -17,6 +17,27 @@ public class StringConcatenation
 
         String str7 = str2.concat(str3);
         System.out.println(str1 == str7); //false since we are concatenating using built in method
+
+        String str8 = str1 + str5 + 100 + 99; //we can concat multiple strings using + unlike with concat().
+        System.out.println(str8);//JavaHyder10099
+
+        String str9 = null;
+        //String str10 = str5.concat(str9); //cannot concat with null value using concat(), it Throws nullpointer Exception
+        
+        String str10 = str5+ str9;
+        //Can concat null value using + operator
+        System.out.println(str10); //Hydernull
+
+        //String str11 = str1.concat(100);
+        //Cannot concat string with int using concat()
+
+        String str11 = str1 + "Hyder" + 99 + 100;
+        System.out.println(str11); //JavaHyder99100
+
+        String str12 = 100 + 99 + str1 + "Hyder";
+        System.out.println(str12); //199JavaHyder - Addition if int is done first and concatenated to string
+
+
     }
 }
 
